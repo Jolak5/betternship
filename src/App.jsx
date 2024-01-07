ef4cfb76c7cebdbbc10a4b67c9e670dd86a27167
jolak5 <kayodej4@gmail.com>
2024-01-07 17:07:09 +0100

    Setup initial repository

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="justify-items-center h-100%">
+      <h1 className="text-3xl font-bold underline align-middle">Betternship</h1>
+    </div>
+  );
+}
+
+export default App;
